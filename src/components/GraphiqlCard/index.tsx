f948894f094ae4fd7368d0484dc90df825fd2969
Anurag Sharma <anurags92@gmail.com>
2020-08-05 01:15:11 +0530

    Added graphiql

--- src/components/GraphiqlCard/index.tsx
+++ src/components/GraphiqlCard/index.tsx
@@ -1,84 +1,23 @@
 import React, {useState} from 'react';
 import GraphiQL from 'graphiql';
+import {Row, Col, Select, DatePicker} from 'antd';
+
 import 'graphiql/graphiql.css';
 
 const URL = 'https://play.dgraph.io/graphql';
 
-const queries = [
-  {
-    name: 'Filmed in San Francisco',
-    queryString: `query {
-	queryLocation(
-		filter: { name: {eq: "San Francisco"} }
-	) {
-		name
-		featured_in_films (
-			order: { desc: initial_release_date },
-			first: 3
-		) {
-			name
-			initial_release_date
-			featured_locations {
-				name
-			}
-		}
-	}
-}`,
-  },
-  {
-    name: 'Director Films',
-    queryString: `query {
-	queryDirector(
-		filter: { name: {eq: "Tom Hanks"} }
-	) {
-		name
-		films(
-			filter: { initial_release_date: {ge: "1970-01-01"} }
-		) {
-			initial_release_date
-			name
-		}
-	}
-}`,
-  },
-  {
-    name: 'Filter by Full-Text',
-    queryString: `query {
-	queryDirector(
-		filter: { name: { eq: "Steven Spielberg" } }
-	) {
-		name
-		films(
-			filter: {
-				name: { alloftext: "jones indiana" }
-				or: { name: { alloftext: "jurassic park" } }
-			}
-		) {
-			name
-		}
-	}
-}`,
-  },
-  {
-    name: 'Sort by Date',
-    queryString: `query {
-	queryDirector(
-		filter: { name: { allofterms: "steven spielberg" } }
-	) {
-		name
-		films(
-			order: { asc: initial_release_date }
-		) {
-			name
-			initial_release_date
-		}
-	}
-}`,
-  },
-];
+const queryPanel = {
+  width: '80%',
+  height: '200px',
+  minWidth: '100px',
+  alignSelf: 'stretch',
+  boxSizing: 'border-box',
+  backgroundColor: 'gainsboro',
+  padding: '10px 0',
+};
 
 function GraphiqlCard({question, updateQuestion, deleteQuestion}: any) {
-  const [query, setQuery] = useState(queries[0].queryString);
+  const [query, setQuery] = useState('');
 
   const graphQLFetcher = (graphQLParams: any) => {
     return fetch(URL, {
@@ -89,29 +28,15 @@ function GraphiqlCard({question, updateQuestion, deleteQuestion}: any) {
   };
 
   return (
-    <>
-      <div className="query-panel">
-        {queries.map(q => (
-          <div
-            key={q.name}
-            className={
-              'query-item ' + (q.queryString === query ? 'active' : '')
-            }
-            onClick={() => setQuery(q.queryString)}
-          >
-            {q.name}
-          </div>
-        ))}
-      </div>
-
-      <div className="graphiql-panel">
-        <GraphiQL
-          fetcher={graphQLFetcher}
-          query={query}
-          onEditQuery={(q: any) => setQuery(q)}
-        />
-      </div>
-    </>
+    <div>
+      {/* <Col span={24}> */}
+      <GraphiQL
+        fetcher={graphQLFetcher}
+        query={query}
+        onEditQuery={(q: any) => setQuery(q)}
+      />
+      {/* </Col> */}
+    </div>
   );
 }
 
