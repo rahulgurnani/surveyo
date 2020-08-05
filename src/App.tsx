ef7cd6a1abd6fb041eef3afcef288a8e6bef2ae0
rahulgurnani <rahul@dgraph.io>
2020-08-05 13:11:01 +0530

    Add auth support

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,86 @@
-import React, {Component} from 'react';
-import {Row, Col} from 'antd';
+import React, { Component } from 'react';
+import { Row, Col, Button, Spin } from 'antd';
 import './App.css';
 import 'antd/dist/antd.css';
 import './index.css';
 import BigCard from './components/BigCard';
 import FormPage from './components/Form';
 import VizPage from './components/Viz';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
-const client = new ApolloClient({
-  uri: 'https://surveyo.us-west-2.aws.cloud.dgraph.io/graphql',
-  cache: new InMemoryCache(),
-});
+import GqlForm from './components/Form';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import createAuth0Client from "@auth0/auth0-spa-js";
+import { useAuth0 } from "@auth0/auth0-react";
+
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+  ApolloLink,
+} from '@apollo/client';
+import createApolloClient from './apollo_config';
+
+
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router>
-        <Switch>
-          <Route path="/creator">
-            <Row>
-              <Col span={6}></Col>
-              <Col span={12}>
-                <BigCard></BigCard>
-              </Col>
-              <Col span={6}></Col>
-            </Row>
-          </Route>
-          <Route path="/form/:id" children={<FormPage />} />
-          <Route path="/viz/:id" children={<VizPage />} />
-        </Switch>
-      </Router>
-    </ApolloProvider>
-  );
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    getAccessTokenSilently,
+    logout
+  } = useAuth0();
+  // const { isLoading, error } = useAuth0();
+
+  // if (error) {
+  //   return <div>Oops... {error.message}</div>;
+  // }
+
+  // if (isLoading) {
+  //   // TODO(rahul): add a loading component
+  // }
+  console.log(isAuthenticated);
+  const client = createApolloClient(user);
+  return (isLoading) ?
+    (<div>
+      <Spin></Spin>
+    </div>) :
+    (isAuthenticated) ?
+      (<ApolloProvider client={client}>
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <div>
+                  <h1>
+                    You are logged in
+                  </h1>
+                </div>        
+              </Route>
+              <Route path="/creator">
+                <Row>
+                  <Col span={6}></Col>
+                  <Col span={12}>
+                    <BigCard></BigCard>
+                  </Col>
+                  <Col span={6}></Col>
+                </Row>
+              </Route>
+              <Route path="/form/:id" children={<FormPage />} />
+              <Route path="/viz/:id" children={<VizPage />} />
+            </Switch>
+          </Router>
+        </ApolloProvider>
+      )
+      :
+      <Button
+          onClick={() => getAccessTokenSilently().then((token: String) => { console.log("authenticated ", token) }).catch((error: Error) =>  { console.log(error); loginWithRedirect();})}
+        >
+          Log in
+    </Button>
+      
 }
 
 export default App;
